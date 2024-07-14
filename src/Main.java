b5b9d434aad1f4d0d3101039d5dbd044b79bf622
roilevi123 <roi9@post.bgu.ac.il>
2024-07-14 21:27:11 +0300

    wr4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import control.BoardGame;
-import control.BoardLevels;
 import model.tiles.Tile;
 import model.tiles.units.players.Player;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        BoardLevels p=new BoardLevels();
-
 
     }
 }
